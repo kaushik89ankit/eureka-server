f81f0e1c0d49a0b5e787012c8bc5132e1732ff69
ankit.kaushik <kaushik89ankit@gmail.com>
2019-06-04 02:05:37 +0530

    Added a sample class

---
+++ src/main/java/com/disha/pmg/eureka_server/sampleClass.java
@@ -0,0 +1,15 @@
+package com.disha.pmg.eureka_server;
+
+public class sampleClass {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
